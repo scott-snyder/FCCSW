2551e81b5621665d97d350535ba3d9ad5c2096da
sss <sss@karma>
2026-03-07 11:08:42 -0500

    Refresh of createCaloCells

--- Examples/options/geant_fullsim_fccee_lar_pgun.py
+++ Examples/options/geant_fullsim_fccee_lar_pgun.py
@@ -152,7 +152,7 @@ createEcalBarrelCellsStep1.calibTool = calibEcalBarrel
 createEcalBarrelCellsStep1.addCellNoise = False
 createEcalBarrelCellsStep1.filterCellNoise = False
 # todo: add when update on cvmfs
-createEcalBarrelCellsStep1.addPosition = True
+createEcalBarrelCellsStep1.addPosition = False
 createEcalBarrelCellsStep1.hits = "ECalBarrelPositionedHits"
 createEcalBarrelCellsStep1.cells = "ECalBarrelCellsStep1"
 ApplicationMgr().TopAlg += [createEcalBarrelCellsStep1]
@@ -197,7 +197,7 @@ ApplicationMgr().TopAlg += [createEcalBarrelPositionedCells]
 createHcalBarrelCells = CreateCaloCells("CreateHCaloCells")
 createHcalBarrelCells.doCellCalibration = True
 createHcalBarrelCells.calibTool = calibHcells
-createHcalBarrelCells.addPosition = True
+createHcalBarrelCells.addPosition = False
 createHcalBarrelCells.addCellNoise = False
 createHcalBarrelCells.filterCellNoise = False
 createHcalBarrelCells.hits = "HCalBarrelPositionedHits"
